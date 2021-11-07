600ea0db1dad4d51a03fef5bce8378467bfd6f8c
CPittman96 <82690202+CPittman96@users.noreply.github.com>
2021-11-07 14:13:23 -0700

    Added some queries to Query file

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,43 @@
-USE SKS
+-- DATA2201 Group Project
+-- Queries for SKS Database
+-- Janine Neville, Connor Pittman, Alex Tran
 
-SELECT * from Customers;
+-- 1
+-- "User Story" (Display all current managers who have hired other current employees) [subquery in a WHERE statement]
+
+USE SKS;
+
+SELECT EmployeeID, FirstName, LastName
+FROM Employee
+WHERE EmployeeID IN (SELECT ManagerID FROM Employee);
+
+
+-- 2 
+-- Special training is being offered for all employees hired after April 1st 2020 that work at the Country Hills location. Only display employees that qualify for this special training.
+
+USE SKS;
+
+SELECT E.EmployeeID, E.FirstName, E.LastName
+FROM Employee E JOIN EmployeeLocation EL
+	ON E.EmployeeID = EL.EmployeeID
+WHERE E.HireDate >= '2020-04-01' AND EL.LocationID = 5;
+
+-- 11
+-- "User Story" (Display all loans with balances less than $2000 as of October 1st 2021, only showing loans made by a specific account) 
+
+USE SKS;
+
+SELECT LR.LoanID, LR.LoanAmount
+FROM LoanRecord LR JOIN AccountLoan AL
+	ON LR.LoanID = AL.LoanID
+WHERE LR.LoanAmount < 2000 AND PaymentDate <= '2021-10-01'
+GROUP BY PaymentDate
+HAVING AL.AccountID = 1756788;
+
+
+-- 12
+-- "User Story"
+
+USE SKS;
+
+
