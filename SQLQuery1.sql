5483440a9e93588bbf355986c125324c1d411930
CPittman96 <82690202+CPittman96@users.noreply.github.com>
2021-11-18 20:04:30 -0700

    Edited user stories to make them consistent, minor edit to query 1

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,14 +3,15 @@
 -- Janine Neville, Connor Pittman, Alex Tran
 
 -- 1
-
--- This will report will need to include a list of all the management staff who have hired employees in the province of Alberta.  
--- The bank manager wants to display all current managers who have hired other current employees) 
--- SUBQUERY in a WHERE statement
+/**
+	"User Story" 
+	The bank owner has requested a report that needs to include a list containing all employees along with their respective managers, including EmployeeID and name for both the employee and their manager.  
+	This list should be organized by Manager Name then Employee Name.
+**/
 
 USE SKS;
 
-SELECT EmployeeID, FirstName + ' ' + LastName AS 'Employee Name', ISNULL(ManagerID, 'N/A') AS ManagerID,
+SELECT EmployeeID, FirstName + ' ' + LastName AS 'Employee Name', ManagerID,
 	CASE
 		WHEN ManagerID = EmployeeID THEN 'N/A'
 		WHEN ManagerID = 111 THEN 'Bradley King'
@@ -22,10 +23,12 @@ FROM Employee
 ORDER BY 'Manager Name' ASC, 'Employee Name' ASC;
 
 -- 2 
--- "User Story" (Special training is being offered for all employees that work at the 'Downtown' bank location and have worked for the bank 
--- for at least three years. The manager will query the database to prepare a list of all employees, sorted by EmployeeID, with employees 
--- that qualify for the training being listed first.)
--- CASE statement
+/**
+	"User Story" 
+	Special training is being offered for all employees that work at the 'Downtown' bank location and have worked for the bank 
+	for at least three years. The manager will query the database to prepare a list of all employees, sorted by EmployeeID, with employees 
+	that qualify for the training being listed first.
+**/
 
 USE SKS;
 
@@ -38,9 +41,11 @@ FROM Employee
 ORDER BY 'Training Eligible' DESC, EmployeeID ASC;
 
 -- 3
--- "User Story" (A loan officer is looking to contact prospective loan customers who are already existing account holders at the SKS Bank.
--- The loan officer wants to display all chequing accounts with balances greater than $500 that have never made any loans, order result by account balance)
--- LEFT JOIN clause
+/**
+	"User Story" 
+	A loan officer is looking to contact prospective loan customers who are already existing account holders at the SKS Bank.
+	The loan officer wants to display all chequing accounts with balances greater than $500 that have never made any loans, order result by account balance.
+**/
 
 USE SKS;
 
@@ -52,8 +57,10 @@ ORDER BY A.Balance;
 
 
 -- 4
-/*  "User Story" (The banks's owner wants to know how much money has been lent out in the form of loans to date.)
-*/
+/** 
+	"User Story" 
+	The banks's owner wants to know how much money has been lent out in the form of loans to date.
+**/
 
 USE SKS;
 
@@ -66,10 +73,10 @@ JOIN LoanPayments LP
 WHERE Lo.BranchID = '2';
 
 -- 5
-
-/*
-"User Story" (The Bank manager wants to know how long its employees have been with the Bank, since their date of hire through to the current date.)
-*/
+/**
+	"User Story" 
+	The Bank manager wants to know how long its employees have been with the Bank, since their date of hire through to the current date.
+**/
 
 USE SKS;
 
@@ -77,14 +84,14 @@ SELECT FirstName + ' ' + LastName AS Employee, DATEDIFF(DD,HireDate, SYSDATETIME
 FROM Employee
 
 -- 6
-
-/*
-"User Story" (An irrate customer calls to complain about a bank employee, the customer is very upset and is yelling and refuses to give their name
-or phone number during the call, but does give their Account ID number to the bank manager during the call.  The manager was unsuccessful in calming the customer down and 
-resolving their complaint and the customer hung up the phone.  The manager would like an opportunity to remedy the situation so they will need to 
-query the SKS bank database and find out who that account (Account ID 120) is held by, and get a contact phone number to call the customer back and get
-further information and try to help the customer.
-*/
+/**
+	"User Story" 
+	An irrate customer calls to complain about a bank employee, the customer is very upset and is yelling and refuses to give their name
+	or phone number during the call, but does give their Account ID number to the bank manager during the call.  The manager was unsuccessful in calming the customer down and 
+	resolving their complaint and the customer hung up the phone.  The manager would like an opportunity to remedy the situation so they will need to 
+	query the SKS bank database and find out who that account (Account ID 120) is held by, and get a contact phone number to call the customer back and get
+	further information and try to help the customer.
+**/
 
 USE SKS;
 
@@ -95,11 +102,11 @@ JOIN CustomerAccount CA
 WHERE CA.AccountID = '120';
 
 -- 7
-
-/*
-"User Story" (The bank manager asks a loan officer to contact the customer for LoanID 108 to offer them a special incentive to switch their loan balance
-to a more flexible personal line of credit.  The loan officer wants to know who to contact for that specific Loan.)
-*/
+/**
+	"User Story" 
+	The bank manager asks a loan officer to contact the customer for LoanID 108 to offer them a special incentive to switch their loan balance
+	to a more flexible personal line of credit.  The loan officer wants to know who to contact for that specific Loan.
+**/
 
 USE SKS;
 
@@ -113,13 +120,16 @@ WHERE LP.LoanID = '108' AND LP.PaymentNumber = '2'
 
 
 -- 8
-/** "User Story" (A customer has arrived at the bank displaying their receipt for a deposit, explaining how they visited Branch ID 5 
-and made a deposit of $1030.75 to their account (AccountID 114) on October 15, 2021, however they have now examined their account statement for October
-and their payment is not listed.  Upon examination, the receipt is proven real.  To correct this, the employee can search payments on that date in 
-that amount, to determine which account the payment was deposited into, and correct the transaction. The employee will then need 
-to ensure the AccountTransaction table is updated to show the payment is reflected in the correct account).  The employee determines 
-that the payment mistakenly went into AccountID 113.  Now that employee can update the database to fix the record for that
-transaction to reflect the correct customer's Account ID (114).  **/
+/** 
+	"User Story" 
+	A customer has arrived at the bank displaying their receipt for a deposit, explaining how they visited Branch ID 5 
+	and made a deposit of $1030.75 to their account (AccountID 114) on October 15, 2021, however they have now examined their account statement for October
+	and their payment is not listed.  Upon examination, the receipt is proven real.  To correct this, the employee can search payments on that date in 
+	that amount, to determine which account the payment was deposited into, and correct the transaction. The employee will then need 
+	to ensure the AccountTransaction table is updated to show the payment is reflected in the correct account.  The employee determines 
+	that the payment mistakenly went into AccountID 113.  Now that employee can update the database to fix the record for that
+	transaction to reflect the correct customer's Account ID (114).  
+**/
 
 /**  Step 1 finding the missing deposit **/
 USE SKS
@@ -141,9 +151,12 @@ WHERE TransactionDate='2021-10-15';
 
 
 -- 9
-/**  "User Story" (You are an assistant to a bank manager.  The manager has asked you to create 
-a list of all employees including their address, who have worked for the bank in the province of Alberta for 
-over 5 years as of January 1, 2022, so that the bank can mail to them a 5 years of service award cheque.)  **/
+/**
+	"User Story" 
+	You are an assistant to a bank manager.  The manager has asked you to create 
+	a list of all employees including their address, who have worked for the bank in the province of Alberta for 
+	over 5 years as of January 1, 2022, so that the bank can mail to them a 5 years of service award cheque.  
+**/
 
 USE SKS;
 
@@ -153,9 +166,12 @@ WHERE HireDate>'2017-01-01';
 
 
 -- 10
-/** "User Story (The bank has noticed they are having trouble retaining entry level employees.
-In an attempt to encourage employees to stay, the bank wants to increase their lowest paid employees'
-hourly pay rates.)  **/
+/**
+	"User Story"
+	The bank has noticed they are having trouble retaining entry level employees.
+	In an attempt to encourage employees to stay, the bank wants to increase their lowest paid employees'
+	hourly pay rates.
+**/
 
 USE SKS;
 
@@ -165,9 +181,12 @@ WHERE Payrate < 25;
 
 
 -- 11
--- SKS National Bank management wants a list of all employees that are listed as personal bankers for customers of the bank.
--- Make sure to sort the resulting table by CustomerID and include the following columns: CustomerID, Customer Name, BranchID, Branch Name, Preferred Employee, and EmployeeID. Sort the results by CustomerID.
--- Muliple INNER JOIN clauses
+/**
+	"User Story" 
+	SKS National Bank management wants a list of all employees that are listed as personal bankers for customers of the bank.
+	Make sure to sort the resulting table by CustomerID and include the following columns: CustomerID, Customer Name, BranchID, Branch Name, Preferred Employee, and EmployeeID. 
+	Sort the results by CustomerID.
+**/
 
 USE SKS;
 
